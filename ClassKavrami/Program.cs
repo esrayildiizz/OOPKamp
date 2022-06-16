f93c1832b2a847340efcd42b45b649d372defe8c
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-16 16:22:42 +0300

    Add project files.

---
+++ ClassKavrami/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassKavrami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Bir class tanımlamasında tanımlanan yerde aynı isimde birden fazla class tanımlanamaz.
+
+            //Bir class tanımlandığında o class adı bir türdür. Haliyle o türü kullanabilmek için direkt olarak class adını kullanmamız yeterlidir.
+        }
+    }
+
+    class OrnekModel
+    {
+
+    }
+}
+
