74b081457d7dee1ca147307f65f8e8e18e99ded1
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-16 19:16:02 +0300

    Field kavramından bahsedildi.

--- ClassKavrami/Program.cs
+++ ClassKavrami/Program.cs
@@ -14,12 +14,26 @@ namespace ClassKavrami
             //Bir class tanımlamasında tanımlanan yerde aynı isimde birden fazla class tanımlanamaz.
 
             //Bir class tanımlandığında o class adı bir türdür. Haliyle o türü kullanabilmek için direkt olarak class adını kullanmamız yeterlidir.
+
+
+            //#region Field
+            //Nesne içerisinde değer tutmamızı sağlayan alanlardır.
+
+            MyClass m1 = new MyClass(); //MyClass sınıfından m1 adlı bir nesne oluşturduk.
+            
+
         }
-    }
 
-    class OrnekModel
+    }
+    class MyClass
     {
-
+       public int a;
+       public string b; 
     }
+
+
+
+
+   
 }
 
