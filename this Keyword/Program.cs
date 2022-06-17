94574625938d74932af775ec93ac2d1f0d731505
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-17 18:48:08 +0300

    this  keyword incelendi.

---
+++ this Keyword/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace this_Keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region this Keyword
+            #region 1-Sınıfın nesnesini temsil eder.
+            MyClass m1 = new MyClass();
+            MyClass m2 = new MyClass();  
+
+
+            #region 2-Aynı isimde field ile metot parametrelerini ayırmak için kullanılır.
+            #region 3-Bir Constructer'dan başka bir Constructer'ı çağırmak için kullanılır.
+        }
+    }
+    class MyClass
+    {
+        public void X()
+        {
+            this.X();
+        }
+    }
+}
