c1faba30bb23a9a9740f61e4850d55cc5cf8f2cf
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-17 14:53:11 +0300

    Full property aciklamasi yapildi.

--- ClassKavrami/Program.cs
+++ ClassKavrami/Program.cs
@@ -12,28 +12,54 @@ namespace ClassKavrami
         {
 
             //Bir class tanımlamasında tanımlanan yerde aynı isimde birden fazla class tanımlanamaz.
-
             //Bir class tanımlandığında o class adı bir türdür. Haliyle o türü kullanabilmek için direkt olarak class adını kullanmamız yeterlidir.
 
 
             //#region Field
             //Nesne içerisinde değer tutmamızı sağlayan alanlardır.
 
-            MyClass m1 = new MyClass(); //MyClass sınıfından m1 adlı bir nesne oluşturduk.
-            
+            //MyClass m1 = new MyClass(); //MyClass sınıfından m1 adlı bir nesne oluşturduk.
+            //m1.a = 20;
+            //MyClass m2 = new MyClass();
+            //m2.a = 30;
 
         }
 
     }
     class MyClass
     {
-       public int a; //Public erişilebilir anlamına gelir.
-       public string b; 
+        int yasi; 
+       string b;
+
+        //#region Full Property
+        //Property hangi türden bir field'ı temsil ediyorsa o türden olmalıdır.
+        //Propertyler temsil ettikleri field'ların isimlerinin başharfi büyük olacak şekilde isimlendirilir.
+
+        public int Yasi
+        {
+            get
+            {
+                //Property üzerinde değer talep edildiğinde bu blok tetiklenir.
+                //Yani değer buradan gönderilir.
+                return yasi;
+            }
+            set
+            {
+
+
+            }
+        }
+
+
+
+
+
+
     }
 
 
 
 
-   
+
 }
 
