01fe51a752437080c256ea7e1c2b04c70a5b52f7
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-19 11:47:25 +0300

    Deconstruct tanimlamasi yapildi.

---
+++ DeconstructTanimlama/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeconstructTanimlama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person
+            {
+                Name = "Esra",
+                Age = 23
+            };
+
+            
+            var (x, y) = person;
+
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }    
+
+        public int Age { get; set; } 
+
+        public void Deconstruct( out string name, out int age)
+        {
+            name = Name;
+            age=Age;    
+        }
+    }
+}
