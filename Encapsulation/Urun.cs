9ad0c91ba86da13a12ac8f940416627cb959f348
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-21 12:36:47 +0300

    Encapsulation Ornegi

---
+++ Encapsulation/Urun.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Urun
+    {
+
+        private string adi;
+      
+        public string Adi
+        {
+            get
+            {
+                return adi;
+            }
+            set
+            {
+                adi= value; 
+            }
+        }
+
+        private string soyadi;
+
+        public string Soyadi
+        {
+            get
+            {
+                return soyadi;
+            }
+            set
+            {
+                soyadi= value;
+            }
+        }
+
+    }
+
+}
