341f91d4a77109ce58f7dab914073369674832d2
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-21 15:44:51 +0300

    Inheritance ornegi yapildi.

---
+++ Inheritance/Memur.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Memur:Insan
+    {
+        public string Okulu { get; set; }
+        public DateTime IseGirisTarihi { get; set; }
+    }
+}
---
+++ Inheritance/Ogretmen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Ogretmen:Memur
+    {
+        
+        public string Brans { get; set; }   
+        public string Sinif { get; set; }
+        
+
+
+    }
+}
