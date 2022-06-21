8da519bda71310f9d2f29581ab267713e77de192
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-21 18:44:05 +0300

    Virtual tanimi yapildi.

---
+++ Polymorphizm/Yemek.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance2
+{
+    internal class Yemek
+    {
+        public string PismeSuresi { get; set; } 
+        public string Malzemeler { get; set; }  
+        public string AciTipi { get; set; } 
+        public string Asci { get; set; }    
+        public string PismeSekli { get; set; }  
+        public string PismeYeri { get; set; }   
+
+
+         public  virtual string SunumSekli()
+        {
+            return "Yanında az pilav ve salata ile.";
+         }
+
+        /*
+         * virtual: virtual komutu ile işaretlenmiş metot isteğe bağlı olarak türetilen 
+         sınıflarda değiştirilebilir anlamına gelmektedir. Yani bu metot ezilebilir denilmiş olur.
+        
+         */
+    }
+}
