c0db540ff7fe8f55dd21eabe2d391265b10b90f7
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-22 14:44:44 +0300

    Siniflar eklendi.

---
+++ Abstract/MuzikAletleri.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    internal class MuzikAletleri
+    {
+        /*
+         * Abstract(soyut):
+         * Abstract sınıflar sadece miras verilmek için tasarlanan sınıflardır.
+         * Abstractlar sınıfa özel değil, bir property , bir metot veya herhangi bir eleman abstract olabilir.
+         *
+         */
+
+        public string Cal()
+        {
+            return "Müzik Aleti çalınıyor.";
+        }
+
+
+
+
+    }
+}
