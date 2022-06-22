2d1fee7c2fd88dee0e4bf3e1dad4930e727bdeaf
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-22 12:38:33 +0300

    Enum ornegi yazildi.

---
+++ Enumeration/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enumeration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Ogrenci ogr= new Ogrenci();
+            ogr.Adi = " Esra";
+            ogr.Soyadi = "YILDIZ";
+            ogr.Alani = Alan.Sayısal;
+
+
+
+            Ogrenci ogr2 =new Ogrenci();
+            ogr2.Alani = Alan.Sayısal;
+
+
+
+        }
+    }
+}
---
+++ Enumeration/Ogrenci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeration
+{
+    internal class Ogrenci
+    {
+        public string Adi { get; set; } 
+        public string Soyadi { get; set; }  
+        public  Alan  Alani  { get; set; }  
+        
+    }
+
+   
+    // String şeklinde enumların içerisine değer yazılmaz.
+    // Enum içerisindeki seçenek bir kelimeden fazla olsa bile kelimeler arasında boşluk bırakamaz.
+
+    public enum Alan
+    {
+        Sayısal,
+        Sözel,
+        EşitAgırlık,
+        dil,
+
+    }
+}
